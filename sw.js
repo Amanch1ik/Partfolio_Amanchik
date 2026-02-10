b01c9902001a30b9ed999671867584ada05010f3
Amanch1ik <aman4ikaitbekov@icloud.com>
2026-02-10 13:43:17 +0600

    fix: darker grey backgrounds

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'portfolio-v1';
+const CACHE_NAME = 'portfolio-v3';
 const ASSETS = [
     './',
     './index.html',
@@ -11,11 +11,21 @@ const ASSETS = [
 ];
 
 self.addEventListener('install', (event) => {
+    self.skipWaiting();
     event.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            // Use individual add for resilience
             return Promise.allSettled(
                 ASSETS.map(url => cache.add(url).catch(err => console.log('Failed to cache:', url, err)))
+            );
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
             );
         })
     );
